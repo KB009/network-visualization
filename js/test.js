f894f23d3348fe6f8769f36994f303bbfd7fe3ba
kikinna <zakopcanova.k@gmail.com>
2015-11-30 20:48:44 +0100

    Examples of retrieving info from menu II /fix #19
    Getting info from sliders and buttons (except for colorScheme)

--- js/test.js
+++ js/test.js
@@ -20,4 +20,37 @@ $(document).ready(function() {
     $('#test-' + $(this).parent().attr('id')).html(mapTo)
   }).trigger('change')
 
+  $('#slider-flows').on('slidechange', function() {
+    $('#test-slider-flows-min').html(
+      $('#slider-flows').slider('values', 0)
+    )
+
+    $('#test-slider-flows-max').html(
+      $('#slider-flows').slider('values', 1)
+    )
+  }).trigger('slidechange')
+
+  $('#slider-dataVolume').on('slidechange', function() {
+    $('#test-dataVolume-min').html(
+      $('#slider-dataVolume').slider('values', 0)
+    )
+    $('#test-dataVolume-max').html(
+      $('#slider-dataVolume').slider('values', 1)
+    )
+  }).trigger('slidechange')
+
+  $('#slider-nodeSize').on('slidechange', function() {
+    $('#test-slider-nodeSize').html(
+      $('#slider-nodeSize').slider('value')
+    )
+  }).trigger('slidechange')
+
+  $('#button-order-prefix').click(function() {
+    $('#test-button-order-prefix').html('bum')
+  })
+
+  $('#button-balance').click(function() {
+    $('#test-button-balance').html('BUM')
+  })
+
 })
