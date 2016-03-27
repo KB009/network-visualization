dc8973f7759047c760eafe4e0ba62efc906f0956
kikinna <zakopcanova.k@gmail.com>
2016-03-27 19:01:35 +0200

    Fix #26 + mapped slider values to JSON values of Menu
    * Created Menu object and corresponding JSON menu.json
    * Fixed position of pin button and size of menu
    * Mapped slider values to Menu and all values are controlled by
    Menu.set/get methods.
    * Slider values of menu have been successfully tested

--- js/menu.js
+++ js/menu.js
@@ -8,10 +8,190 @@
     * zkontrolovat na co potrebuji checkbox-mapto id
  */
 
-var Menu = {}
+var menuEvent = new CustomEvent('menuUpdate');
+
+// document.getElementById('menu').addEventListener('menuUpdate', function(e) {
+//   console.log("Detected event");
+// })
+
+var Menu = {
+  mapColorTo: "flows",
+  mapNodeTo: "ip",
+  mapTo: "nodes",
+  colorScheme: 1,
+  minFlowNum: 0, 
+  maxFlowNum: 150, 
+  minDataVolume: 1, 
+  maxDataVolume: 200, 
+  nodeSize: 45,
+
+  loadValuesFromJson: function() {
+
+  },
+
+  setMapColorTo: function(newValue) {
+    mapColorTo = newValue;
+
+    // throw event
+    // var menuEvent = new CustomEvent('menuUpdate');
+    console.log("Jsem v setMapColorTo");  
+    document.getElementById("menu").dispatchEvent(menuEvent);
+    // window.dispatchEvent(menuEvent);
+    // update JSON
+    // data.mapColorTo = newValue;
+
+  },
+  getMapColorTo: function() {
+    return mapColorTo;
+  },
+  setMapNodeTo: function(newValue) {
+    mapNodeTo = newValue;
+  },
+  getMapNodeTo: function() {
+    return mapNodeTo;
+  },
+  setMapTo: function(newValue) {
+    mapTo = newValue;
+  },
+  getMapTo: function() {
+    return mapMapTo;
+  },
+  setColorScheme: function(newValue) {
+    colorScheme = newValue;
+  },
+  getColorScheme: function() {
+    return colorScheme;
+  },
+
+  // ---------- FLOW NUM -------------------------------------------------
+  setFlowNum: function(minValue, maxValue) { // *
+    minFlowNum = minValue;
+    maxFlowNum = maxValue;
+    // set values of sliders in case the change doesnt come from them
+    if ($('#slider-flows').slider('values', 0) != minValue) {
+      $('#slider-flows').slider('values', 0, minValue);
+      $('#slider-value-flow-min').html(minValue);
+    }
+    if ($('#slider-flows').slider('values', 1) != maxValue) {
+      $('#slider-flows').slider('values', 1, maxValue);
+      $('#slider-value-flow-max').html(maxValue);
+    }
+    
+    var evt = new CustomEvent('menuUpdate', { detail: 'flowNum'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("Changed min/max flow value: ", minFlowNum, maxFlowNum);
+  },
+  setMinFlowNum: function(minValue) {
+    minFlowNum = minValue;
+    
+    if ($('#slider-flows').slider('values', 0) != minValue) {
+      $('#slider-flows').slider('values', 0, minValue);
+      $('#slider-value-flow-min').html(minValue);
+    }
+    
+    var evt = new CustomEvent('menuUpdate', { detail: 'flowNum'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("Changed min flow value: ", minFlowNum);
+  },
+  getMinFlowNum: function() {
+    return minFlowNum;
+
+  },
+  setMaxFlowNum: function(maxValue) {
+    maxFlowNum = maxValue;
+
+    if ($('#slider-flows').slider('values', 1) != maxValue) {
+      $('#slider-flows').slider('values', 1, maxValue);
+      $('#slider-value-flow-max').html(maxValue);
+    }
+
+    var evt = new CustomEvent('menuUpdate', { detail: 'flowNum'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("Changed max flow value: ",  maxFlowNum);
+  },
+  getMaxFlowNum: function() {
+    return maxFlowNum;
+  },
+
+  // ---------- DATA VOLUME -------------------------------------------------
+  setDataVolume: function(minValue, maxValue) { // *tested OK
+    minDataVolume = minValue;
+    maxDataVolume = maxValue;
+
+    if ($('#slider-dataVolume').slider('values', 0) != minValue) {
+      $('#slider-dataVolume').slider('values', 0, minValue);
+      $('#slider-value-volume-min').html(minValue);
+    }
+    if ($('#slider-dataVolume').slider('values', 1) != maxValue) {
+      $('#slider-dataVolume').slider('values', 1, maxValue);
+      $('#slider-value-volume-max').html(maxValue);
+    }
+    
+    var evt = new CustomEvent('menuUpdate', { detail: 'dataVolume'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("I've updated data volume to ", minDataVolume, maxDataVolume);
+  },
+  setMinDataVolume: function(minValue) { // *tested OK
+    minDataVolume = minValue;
+
+    if ($('#slider-dataVolume').slider('values', 0) != minValue) {
+      $('#slider-dataVolume').slider('values', 0, minValue);
+      $('#slider-value-volume-min').html(minValue);
+    }
+
+    var evt = new CustomEvent('menuUpdate', { detail: 'dataVolume'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("I've updated MIN data volume to ", minDataVolume);
+  },
+  getMinDataVolume: function() { // *tested OK
+    return minDataVolume;
+  },
+  setMaxDataVolume: function(maxValue) { // *tested OK
+    maxDataVolume = maxValue;
+
+    if ($('#slider-dataVolume').slider('values', 1) != maxValue) {
+      $('#slider-dataVolume').slider('values', 1, maxValue);
+      $('#slider-value-volume-max').html(maxValue);
+    }
+
+    var evt = new CustomEvent('menuUpdate', { detail: 'dataVolume'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("I've updated MAX data volume to ", maxDataVolume);
+  },
+  getMaxDataVolume: function() { // *tested OK
+    return maxDataVolume;
+  },
+
+  // ---------- NODE SIZE -------------------------------------------------
+  setNodeSize: function(newValue) { // *tested OK
+    nodeSize = newValue;
+
+    $('#slider-nodeSize').slider('value', newValue);
+    $('#slider-value-nodeSize').html( newValue );
+
+    var evt = new CustomEvent('menuUpdate', { detail: 'nodeSize'});
+    document.getElementById("menu").dispatchEvent(evt);
+
+    console.log("I've updated node size to ", nodeSize);
+  },
+  getNodeSize: function() { // *tested OK
+    return nodeSize;
+  },
+}
+
+
+// ********** Renderovani okna ***************
 Menu.render = function() {
 
-  var menuwrapper = $('<div/>', { 'id':'topmenu2'});
+  // var menuwrapper = $('<div/>', {''})
+  var menuwrapper = $('<div/>', { 'id':'topmenu'});
+
 
   // RADIOBUTTON / Barva
   var radioDisplay = $('<div/>', {
@@ -141,11 +321,47 @@ Menu.pin = function() {
 
   return pinButton;
 }
+// **********************************************************************
+
 
 $(document).ready(function() {
+
+  var menu = {
+    "map-color-to": "flows",
+    "map-node-to" : "ip",
+    "mapTo" : "nodes",
+    "color-scheme" : 1,
+    "min-num-of-flows" : 0,
+    "max-num-of-flows" : 150,
+    "min-data-volume" : 1,
+    "max-data-volume" : 200,
+    "node-size" : 45
+  }
+
+  for (var i in menu) {
+      console.log(menu.mapTo);
+      console.log(menu[i]);
+    }
+
+// ------- WORKS JUST FINE -------
+//   $.getJSON('menu.json', function(data) {
+//     console.log("jsem tu");
+//     for (var i in data) {
+//       console.log(data.mapTo);
+//       console.log(data[i]);
+//     }
+// });  
+
+  
+
+
   // $('#column1').prepend(Menu.render())
-  $('#topmenu').prepend(Menu.render())
+  // 
+  // $('#menu').prepand
+  $('#menu').append(Menu.render())
   $('#menu').append(Menu.pin());
+
+
 
   $('#customColor').button();
   $('#button-prefix-order').button();
@@ -153,14 +369,22 @@ $(document).ready(function() {
   $('#button-pin').button();
   $('#Pum').button();
 
-  // Filtrovat
+  
+
+  // ********** S L I D E R / Flow num ***********
   $('#slider-flows').slider({
     max: 150,
     range: true,
     values: [ 0, 150 ],
     slide: function( event, ui ) {
-      $('#slider-value-flow-min').html(ui.values[0]);
-      $('#slider-value-flow-max').html(ui.values[1]);
+      // $('#slider-value-flow-min').html(ui.values[0]);
+      // $('#slider-value-flow-max').html(ui.values[1]);
+      // Menu.setFlowNum(ui.values[0], ui.values[1]);
+
+      
+      Menu.setMinFlowNum(ui.values[0]);
+      Menu.setMaxFlowNum(ui.values[1]);
+      // console.log("pokus" + Menu.getMinFlowNum());
     }
   });
   $('#slider-value-flow-min').html(
@@ -170,13 +394,13 @@ $(document).ready(function() {
     $('#slider-flows').slider('values', 1)
   )
 
+  // ********* S L I D E R / Data Volume **********
   $('#slider-dataVolume').slider({
     max: 200,
     range: true,
     values: [ 1, 200 ],
     slide: function( event, ui ) {
-      $('#slider-value-volume-min').html(ui.values[0]);
-      $('#slider-value-volume-max').html(ui.values[1]);
+      Menu.setDataVolume(ui.values[0], ui.values[1]);
     }
   });
   $('#slider-value-volume-min').html(  // volume vs dataVolume - sjednotit!
@@ -186,14 +410,13 @@ $(document).ready(function() {
     $('#slider-dataVolume').slider('values', 1)
   )
 
-  // Velikost uzlu
+  // ********* S L I D E R / Node size **********
   $('#slider-nodeSize').slider({
     value: 45,
     min: 10,
     max: 70,
     slide: function( event, ui ) {
-      console.debug(ui)
-      $( '#slider-value-nodeSize' ).html( ui.value );
+      Menu.setNodeSize(ui.value);
     }
 
   });
@@ -203,48 +426,6 @@ $(document).ready(function() {
    );
 
 
-})
-
-// $(function() {
-  // $('#radiobutton-display').buttonset();
-  // $('#radiobutton-node').buttonset();
-  // $('#checkbox-mapto').buttonset();
-// });
-
-// ------ SLIDERS ---------
-
-$('#slider-value-flow-min').val(5);
-
-$(function() {
-});
-
-//
-// $('.menu').ready(function() {
-//   alert('Menu loaded!');
-//
-// });
-
-/*  POKUSY NA NASTAVENI INICIALNI HODNOTY
-$( '#slider-nodeSize' ).on( 'slidecreate', function( event, ui ) {
-  $( '#slider-value-nodeSize' ).html( ui.value );
-} );
-
-$( '#slider-value-nodeSize' ).html(  $('#slider-nodeSize').slider('value') );
-
-$( '#div-flows').onLoad(function() {
-  alert('ha');
-  $('#slider-value-nodeSize').html(function() {
-    $('#slider-nodeSize').slider('option', 'max');
-  });
-});
-*/
-
-//
-// // Getter
-// var max = $( '.selector' ).slider( 'option', 'max' );
-//
-// // Setter
-// $( '.selector' ).slider( 'option', 'max', 50 );
 
 $(function () {
   function runEffect() {
@@ -264,19 +445,35 @@ $(function () {
   });
 });
 
-/*
-$('#div-radiobutton-display').click(  function() {
-  // alert('ha');
-  alert('Value: ' + $('input[name=radio]:checked', '#menu-form').val());
-});
 
-var val = $('#slider-nodeSize').slider({
-  change: function(event, ui) {
-          // alert(ui.value);
-      }
-    });
+})
 
-$('#button-balance').click(function(event) {
-  console.log('clicked: ' + event.target);
-}) ;
-*/
+// $(function() {
+  // $('#radiobutton-display').buttonset();
+  // $('#radiobutton-node').buttonset();
+  // $('#checkbox-mapto').buttonset();
+// });
+
+// ------ SLIDERS ---------
+
+
+// function pucButton() {
+//     console.log("puc");
+//     Menu.setFlowNum(10, 100);
+//   }
+// POKUS / nastavit hodnoty slideru externe
+// $("#button-balance").on('click', pucButton);
+// $("#button-balance").click(function() {
+//     console.log("puc");
+//     Menu.setFlowNum(10, 100);
+//   });
+
+//
+// // Getter
+// var max = $( '.selector' ).slider( 'option', 'max' );
+//
+// // Setter
+// $( '.selector' ).slider( 'option', 'max', 50 );
+
+
+
--- js/test.js
+++ js/test.js
@@ -20,37 +20,103 @@ $(document).ready(function() {
     $('#test-' + $(this).parent().attr('id')).html(mapTo)
   }).trigger('change')
 
-  $('#slider-flows').on('slidechange', function() {
-    $('#test-slider-flows-min').html(
-      $('#slider-flows').slider('values', 0)
-    )
 
-    $('#test-slider-flows-max').html(
-      $('#slider-flows').slider('values', 1)
-    )
-  }).trigger('slidechange')
+  // POKUS REAKCE NA CUSTOM EVENT MENUUPDATE
+  // $('#slider-flows').on('menuUpdate', function(e) {
+  //   console.log("Som tu");
+  //   if (e.detail == 'flowNum') {
+  //     $('#test-slider-flows-min').html(
+  //       $('#slider-flows').slider('values', 0)
+  //       )
+  //     $('#test-slider-flows-max').html(
+  //       $('#slider-flows').slider('values', 1)
+  //     )
+  //     console.log("Pum");
+  //   }
+  // })
 
-  $('#slider-dataVolume').on('slidechange', function() {
-    $('#test-dataVolume-min').html(
-      $('#slider-dataVolume').slider('values', 0)
-    )
-    $('#test-dataVolume-max').html(
-      $('#slider-dataVolume').slider('values', 1)
-    )
-  }).trigger('slidechange')
+  // $('#slider-flows').on('slidechange', function() {
+  //   $('#test-slider-flows-min').html(
+  //     $('#slider-flows').slider('values', 0)
+  //   )
 
-  $('#slider-nodeSize').on('slidechange', function() {
-    $('#test-slider-nodeSize').html(
-      $('#slider-nodeSize').slider('value')
-    )
-  }).trigger('slidechange')
+  //   $('#test-slider-flows-max').html(
+  //     $('#slider-flows').slider('values', 1)
+  //   )
+  // }).trigger('slidechange')
+
+  // ***** TRACK DATA VOLUME CHANGE *****
+  // $('#slider-dataVolume').on('slidechange', function() {
+  //   $('#test-dataVolume-min').html(
+  //     $('#slider-dataVolume').slider('values', 0)
+  //   )
+  //   $('#test-dataVolume-max').html(
+  //     $('#slider-dataVolume').slider('values', 1)
+  //   )
+  // }).trigger('slidechange')
+  
+  // ***** TRACK NODE SIZE CHANGE *****
+  // $('#slider-nodeSize').on('slidechange', function() {
+  //   $('#test-slider-nodeSize').html(
+  //     $('#slider-nodeSize').slider('value')
+  //   )
+  // }).trigger('slidechange')
 
   $('#button-prefix-order').click(function() {
     $('#test-button-order-prefix').html('bum')
   })
 
   $('#button-balance').click(function() {
-    $('#test-button-balance').html('BUM')
+    $('#test-button-balance').html('BUM');
+    console.log("puc");
+    Menu.setFlowNum(10, 100);
+    Menu.setDataVolume(33, 133);
+    // console.log(Menu.getMinFlowNum());
   })
+
+
+
+
+// Proc tady nemuzu pristoupit k Menu
+$('#menu').on('menuUpdate', function(e) {
+  console.log("Jsem v menuUpdate event", e.detail);
+
+  switch(e.detail) {
+    case 'flowNum':
+      $('#test-slider-flows-min').html( Menu.getMinFlowNum() )
+      $('#test-slider-flows-max').html( Menu.getMaxFlowNum() )
+      break;
+
+    case 'dataVolume':
+      $('#test-dataVolume-min').html( Menu.getMinDataVolume() )
+      $('#test-dataVolume-max').html( Menu.getMaxDataVolume() )
+      break;
+
+    case 'nodeSize':
+      $('#test-slider-nodeSize').html( Menu.getNodeSize() )
+      break;
+
+    
+
+  }
+  // if (e.detail == 'flowNum') {
+  //     $('#test-slider-flows-min').html(
+  //       $('#slider-flows').slider('values', 0)
+  //       // Menu.getMinFlowNum();
+  //       )
+  //     $('#test-slider-flows-max').html(
+  //       $('#slider-flows').slider('values', 1)
+  //     )
+  //     console.log("Pum");
+  //   }
+
+  // $('#test-slider-flows-min').html(
+  //     $('#slider-flows').slider('values', 0)
+  //   )
+
+  //   $('#test-slider-flows-max').html(
+  //     $('#slider-flows').slider('values', 1)
+  //   )
+}) 
 
 })
